0c2bf72dd64f4f84226c46b3da25174d46d36fe7
steven <stevenvuong96@gmail.com>
2023-06-29 21:27:21 +0800

    add refinitiv newstext summary tool to tooling

--- customized_tools.py
+++ customized_tools.py
@@ -4,6 +4,18 @@
 from langchain.tools import tool
 
 
+@tool("Refinitiv freetext news search tool")
+def refinitiv_freetext_news_summary_tool(input: str, num_weeks_ago: int) -> str:
+    """
+    Queries the Refinitiv News API for news articles related to the free text input
+    which have happened in the last num_weeks_ago.
+    Then summarises the news articles and returns the summaryy or enriched headlines.
+
+    Refinitiv is better for English than Chinese news articles.
+    """
+    return
+
+
 @tool("hsbc knowledge search tool")
 def hsbc_knowledge_tool(input: str) -> str:
     """useful for when you need to answer questions about hsbc related knowledge"""
@@ -18,6 +30,7 @@ def hsbc_knowledge_tool(input: str) -> str:
             - holding an eligible Hong Kong ID or an overseas passport
             - new to HSBC
     """
+
 
 @tool("reject tool", return_direct=True)
 def reject_tool(input: str) -> str:
